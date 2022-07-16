0ecd872b9bb307f070dc4c8e6f8f97481579356a
Diones Marcos <dionesvrr@gmail.com>
2022-07-16 17:57:04 -0300

    estudando

---
+++ Staart/01 - Variaveis e Constates/index.js
@@ -0,0 +1,27 @@
+var nome_do_site = "vendinha da leticia"
+console.log(nome_do_site)
+nome_do_site = "venda da leticia"
+console.log(nome_do_site)
+
+var saldo = 100
+saldo = saldo -15
+console.log(saldo)
+
+/*Constate - Variavel Cujo o valor não pode ser alterado ou seja o valor da varivel posteriormete sera sempre o mesmo*/
+
+const saldo_1 = 100
+console.log(saldo_1)
+
+/*LET - Variaveis escritas com'let' estarão protegidas contra duplicidade. Ou seja, caso existam duas variaveis com o mesmo nome, a execução do programa resultará em um erro. Uma das vantagens de se usar o 'let' ou 'const' que caso esteja mexendo no codigo de outra pessoa você não ira alterar a variavel
+*/
+
+/*Typeof - typeof é usado quando queremos saber o tipo de valor que alguma variável armazena. é util para varivais que esteja vindo do codigo de outra pessoa exemplo ao receber os dados de cartão de credito*/
+
+const variavel_1 = 3 //number
+console.log(typeof variavel_1)
+const variavel_2 = "3" //string
+console.log(typeof variavel_2)
+const variavel_3 = true //bool
+console.log(typeof variavel_3)
+const variavel_4 = "true" //string
+console.log(typeof variavel_4)
---
+++ Staart/06 - Tipo de Operadores pt1/index.js
@@ -0,0 +1,52 @@
+// Tipos Operadores
+// aritmeticos, atribuição, comparação, lógico
+
+/*
+aritmeticos
+soma(+)
+subtração(-)
+multiplicação(*)
+divisão(/)
+*/
+
+console.log(2+3)
+console.log(2-3)
+console.log(2*3)
+console.log(2/3)
+
+/*
+Incremento (++)
+Decremento (--)
+Negação (-)
+Adição (+)
+*/
+
+let saldo = 30
+saldo++
+console.log(saldo)
+
+let saldo_1 = 30
+saldo_1--
+console.log(saldo_1)
+
+//Operadores de Atribuição (=)
+const idade = 18
+
+/*
+x += y
+x -= y
+x *= y
+x /= y
+*/
+
+let saldo_2 = 100
+saldo_2 = 100 - 10 //essa operaçãoes são iguais
+saldo_2 -= 10 //essa operaçãoes são iguais
+console.log(saldo_2)
+
+let saldo_3 = 100
+saldo_3 = 100 + 10 //essa operaçãoes são iguais
+saldo_3 += 10 //essa operaçãoes são iguais
+console.log(saldo_3)
+
+
