085558c101890dfc848100fff8821e1d03dadd50
Diones Marcos <dionesvrr@gmail.com>
2022-08-10 19:20:27 -0300

    JavaScript

---
+++ Udemy/02 - JavaScript - Lógica de Programação/11 - Operação ternária/index.js
@@ -0,0 +1,5 @@
+// montado uma operação ternária
+// (condição) ? 'valor para verdadeiro' : 'valor para falso';
+const pontuacaoUsuario = 1500;
+const nivelUsuario = pontuacaoUsuario >=1000 ? 'usuario VIP' : 'usuario normal';
+console.log(nivelUsuario);
